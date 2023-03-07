3f37d7ccf4f903ef3c26c092f6b7e402e8326766
Bede Skinner-Vennell <bjs185@uclive.ac.nz>
2023-03-07 15:03:12 +1300

    xdfghjkl;

--- Assignment 1/Q6 - CEA Trace.py
+++ Assignment 1/Q6 - CEA Trace.py
@@ -39,9 +39,6 @@ def lge(code_a, code_b):
         if not element_lge(code_a[i], code_b[i]):
             return False
     return True
-
-    
-
             
 def initial_S(domains):
     """Takes a list of domains and returns a set where each element is a
@@ -64,19 +61,32 @@ def minimal_generalisations(code, x):
     """Takes a code (corresponding to a hypothesis) and returns the set of all
     codes that are the minimal generalisations of the given code with respect
     to the given input x."""
+    output = set()
+
     for i in range(len(code)):
-        for j in range(i+1, len(code)):
-            
-
-    
-
+        if element_lge(code[i], x[i]):
+            code_copy = list(code)
+            if code[i] is None:
+                code_copy[i] = x[i]
+            else:
+                code_copy[i] = "?"
+            output.add(tuple(code_copy))
+    return output
 
 def minimal_specialisations(cc, domains, x):
     """Takes a code (corresponding to a hypothesis) and returns the set of all
     codes that are the minimal specialisations of the given code with respect
     to the given input x."""
-    [domain.add() for domain in domains]
-    all_specialisations = input_space(domains)
+    output = set()
+
+    for i in range(len(cc)):
+        if not element_lge(x[i], cc[i]):
+            for new_element in domains[i] - set(x[i]):
+                code_copy = list(cc)
+                code_copy[i] = new_element
+                output.add(tuple(code_copy))
+    
+    return output
 
 
 def cea_trace(domains, D):
@@ -84,42 +94,109 @@ def cea_trace(domains, D):
     S = initial_S(domains)
     G = initial_G(domains)
     # Append S and G (or their copy) to corresponding trace list
-
-    print(G)
-    print(S)
+    S_trace.append(S)
+    G_trace.append(G)
     
     for x, y in D:
         if y: # if positive
-            [G.remove(g) for g in G if not match(g, x)]
+            G = {g for g in G if match(g, x)}
             for s in [s for s in S if not match(s, x)]:
                 S.remove(s)
-                for h in minimal_generalisations(g, x):
-                    if match(h, x):
+                S = S.union(minimal_generalisations(s, x))
+            new_S = set()
+            for s in S:
+                more_general = True
+                for j in S:
+                    if j != s:
+                        more_general = more_general and not lge(s, j)
+                if more_general:
+                    new_S.add(s)
             
+            S = new_S
+
         else: # if negative
-            [S.remove(s) for s in S if match(s, x)]
+            S = {s for s in S if not match(s, x)}
             for g in [g for g in G if not match(g, x)]:
                 G.remove(g)
-                for h in minimal_specialisations(g, domains, x):
-                    if not match(h, x):
+                G = G.union(minimal_specialisations(g, domains, x))
+
+            new_G = set()
+            for g in G:
+                more_specific = True
+                for j in G:
+                    if g != j:
+                        more_specific = more_specific and lge(s, j)
+                if more_specific:
+                    new_G.add(s)
+            
+            G = new_G
             
 
+        S_trace.append(S)
+        G_trace.append(G)
         # Append S and G (or their copy) to corresponding trace list
-
     return S_trace, G_trace
 
 
 if __name__ == "__main__":
+    # domains = [
+    # {'red', 'blue'}
+    # ]
+
+    # training_examples = [
+    #     (('red',), True)
+    # ]
+
+    # S_trace, G_trace = cea_trace(domains, training_examples)
+    # print(len(S_trace), len(G_trace))
+    # print(all(type(x) is set for x in S_trace + G_trace))
+    # S, G = S_trace[-1], G_trace[-1]
+    # print(len(S), len(G))
+
+    # print()
+    # domains = [
+    #     {'T', 'F'}
+    # ]
+
+    # training_examples = []  # no training examples
+
+    # S_trace, G_trace = cea_trace(domains, training_examples)
+    # print(len(S_trace), len(G_trace))
+    # S, G = S_trace[-1], G_trace[-1]
+    # print(len(S), len(G))
+    # print()
+    # domains = [
+    #     ('T', 'F'),
+    #     ('T', 'F'),
+    # ]
+
+    # training_examples = [
+    #     (('F', 'F'), True),
+    #     (('T', 'T'), False),
+    # ]
+
+    # S_trace, G_trace = cea_trace(domains, training_examples)
+    # print(len(S_trace), len(G_trace))
+    # S, G = S_trace[-1], G_trace[-1]
+    # print(len(S), len(G))
+
     domains = [
-    {'red', 'blue'}
+        {"Sunny", "Cloudy", "Rainy"},
+        {"Warm", "Cold"},
+        {"Normal", "High"},
+        {"Strong", "Weak"},
+        {"Warm", "Cool"},
+        {"Same", "Change"}
     ]
 
     training_examples = [
-        (('red',), True)
+        (("Sunny", "Warm", "Normal", "Strong", "Warm", "Same"), True),
+        (("Sunny", "Warm", "High", "Strong", "Warm", "Same"), True),
+        (("Rainy", "Cold", "High", "Strong", "Warm", "Change"), False),
+        (("Sunny", "Warm", "High", "Strong", "Cool", "Change"), True)
     ]
 
     S_trace, G_trace = cea_trace(domains, training_examples)
-    print(len(S_trace), len(G_trace))
-    print(all(type(x) is set for x in S_trace + G_trace))
-    S, G = S_trace[-1], G_trace[-1]
-    print(len(S), len(G))
+
+    [print(str(s) + "\n") for s in S_trace]
+    [print(str(g) + "\n") for g in G_trace]
