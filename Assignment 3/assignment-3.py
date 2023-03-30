72dd4aed79f361446e1514fab53a27fe5cca422d
Bede Skinner-Vennell <bjs185@uclive.ac.nz>
2023-03-31 10:52:31 +1300

    Finished Q2

--- Assignment 3/assignment-3.py
+++ Assignment 3/assignment-3.py
@@ -21,7 +21,35 @@ def linear_regression_1d(input):
     return m, c
 
 
-data = [(1, 4), (2, 7), (3, 10)]
-m, c = linear_regression_1d(data)
-print(m, c)
-print(4 * m + c)
+import numpy as np
+
+def linear_regression(xs, ys):
+    """
+        Takes two numpy arrays as input: the first is the input part of the 
+        training data which is an min array (design matrix), while the second 
+        is the output part of the training data which is a one-dimensional 
+        array (vector) with m elements. Return the one-dimensional array 
+        (vector) θ, with (n + 1) elements, which contains the least-squares 
+        regression coefficients of the features; the first ("extra") value is the intercept.
+    """
+    X = np.c_[np.ones(len(ys)), xs]
+
+    return np.linalg.inv(X.T @ X) @ X.T @ ys
+
+import math
+def logistic_regression(xs, ys, alpha, num_interations):
+
+    sigmoid = lambda x: 1 / (1 + math.exp(-x))
+
+
+
+
+xs = np.arange(5).reshape((-1, 1))
+ys = np.arange(1, 11, 2)
+print(linear_regression(xs, ys))
+
+
+xs = np.array([[1, 2, 3, 4],
+               [6, 2, 9, 1]]).T
+ys = np.array([7, 5, 14, 8]).T
+print(linear_regression(xs, ys))
